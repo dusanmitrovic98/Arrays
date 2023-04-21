d1accf2fc821c90a5ac13e3c5b98e13a00f01640
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-21 18:58:08 +0200

    Updated summary for _elements member variable.

--- Array.cs
+++ Array.cs
@@ -3,7 +3,7 @@
 public class Array<T>
 {
     /// <summary>
-    /// Array elements.
+    /// Array elements, where T represents predefined type for each element in the array.
     /// </summary>
     private T[] _elements;
 }
