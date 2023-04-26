bef20c0d31be66df446de2c8e184043923ae780f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-26 23:17:13 +0200

    Updated Sort method.

--- Array.cs
+++ Array.cs
@@ -254,7 +254,10 @@ public class Array<T> : IArray<T>
 
         for (int i = 0; i < length - 1; i++)
         {
+            for (int j = 0; j < length - i - 1; j++)
+            {
 
+            }
         }
     }
 }
