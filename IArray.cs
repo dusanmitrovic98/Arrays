56cd7670b5fc61e8b3d9609d169067f8f332a070
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-02 22:46:20 +0200

    Updated IArrays.cs file.

--- IArray.cs
+++ IArray.cs
@@ -4,20 +4,5 @@ namespace DataStructures.Collections.Array;
 
 public interface IArray<T>
 {
-    public int Length { get; }
-    public T this[int index] { get; set; }
-    public bool Contains(T value);
-    public void SetValue(T value, int index);
-    public T GetValue(int index);
-    public void ValidateIndex(int index);
-    public void CopyTo(ref Array<T> array, int index = default);
-    public Array<T> Copy(int index = default);
-    public int IndexOf(T value);
-    public void Add(T value);
-    public void Clear();
-    public IEnumerator GetEnumerator();
-    public void Reverse();
-    public void Fill(T value);
-    public void Sort();
-    public void Traverse(Action? action);
+
 }
