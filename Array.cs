d30f9e767b6ad91255f88426e74a703c707f6c2a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-02 22:58:59 +0200

    Updated Clear method.

--- Array.cs
+++ Array.cs
@@ -199,7 +199,8 @@ public class Array<T> : IArray<T>
     /// </summary>
     public void Clear()
     {
-
+        int length = this.Length;
+        this._elements = new T[length];
     }
 
     /// <summary>
