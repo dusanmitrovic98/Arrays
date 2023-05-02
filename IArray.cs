5ef56786bb6ed7551072c1d712fb55a2b8a5cf02
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-02 22:47:02 +0200

    Updated IArrays.cs file.

--- IArray.cs
+++ IArray.cs
@@ -7,4 +7,5 @@ public interface IArray<T>
     public int Length { get; }
     public T this[int index] { get; set; }
     public bool Contains(T value);
+    public void SetValue(T value, int index);
 }
